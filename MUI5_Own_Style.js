4e64e6cbc8bcc120d0e6d5954faf1053cc2278c5
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-12 14:52:36 +0530

    Update MUI5_Own_Style.js

--- MUI5_Own_Style.js
+++ MUI5_Own_Style.js
@@ -20,3 +20,23 @@
       >
         My Unique Button
       </Button>
+
+
+// Creating our Own Styled Button Components !
+
+
+ const BlueButton = styled(Button)({
+    backgroundColor: "tomato",
+    color: "#fff",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+      transition: "0.3s ease-in",
+    },
+
+    "&:disabled": {
+      backgroundColor: "red",
+      color: "white",
+    },
+  });
