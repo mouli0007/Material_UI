be677c32839f9d04c351579d494b0952161e2a6b
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-12 16:14:10 +0530

    Create MUI_Breakpoints_Responsivenes.js

---
+++ MUI_Breakpoints_Responsivenes.js
@@ -0,0 +1,11 @@
+// Example one 
+
+   <Typography variant="h6" sx={{
+            display: {
+              xs: "none",
+              sm: "block",
+            },
+          }}
+        >
+          Mouli VJ
+</Typography>
